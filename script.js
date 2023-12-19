5894fef2bad0630b3cdec54e7220f061dcc34ec1
vibhisan <charu.messi.ab@gmail.com>
2023-12-19 11:38:41 +0530

    drop box fixed

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ let show = document.querySelector(".show-summary")
 
   acc.addEventListener("click", function() {
     var panel = document.querySelector(".panel");
-    if (panel.classList.contains("active")) {
-        panel.classList.remove("active");
-        panel.classList.add("close")
+    if (panel.classList.contains("close")) {
+        panel.classList.remove("close");
+        panel.classList.add("active")
         show.style.display= "none"
         hide.style.display= "block"
 
         
     } else {
-        panel.classList.remove("close")
-        panel.classList.add("active")
+        panel.classList.remove("active")
+        panel.classList.add("close")
 
         hide.style.display= "none"
         show.style.display= "block"
